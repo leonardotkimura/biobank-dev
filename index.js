208582577b3e5a42d2e70ed2bda50daf71d61842
iguau23 <leonardotkimura@gmail.com>
2020-07-30 14:06:06 -0300

    before add ledger-api

--- index.js
+++ index.js
@@ -5,6 +5,10 @@
 'use strict';
 
 const DataContract = require('./lib/data-contract');
+const ProcessRequestContract = require('./lib/process-request-contract');
+const ProcessorContract = require('./lib/processor-contract');
 
 module.exports.DataContract = DataContract;
-module.exports.contracts = [ DataContract ];
+module.exports.ProcessRequestContract = ProcessRequestContract;
+module.exports.ProcessorContract = ProcessorContract;
+module.exports.contracts = [ DataContract, ProcessRequestContract, ProcessorContract ];
