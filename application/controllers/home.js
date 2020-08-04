32bc91cc630dd68d2e79a18a0c8edefc01b92cbb
iguau23 <leonardotkimura@gmail.com>
2020-08-04 16:53:52 -0300

    add dataContract interface

--- application/controllers/home.js
+++ application/controllers/home.js
@@ -1,4 +1,8 @@
-exports.index = function(req, res, next){
-  res.render('home/home', {  });
+const DataContract = require('../contract/dataContract');
+
+exports.index = async function(req, res, next){
+  const dataContract = new DataContract();
+  const data = await dataContract.readData("raw_data", "1");
+  res.render('home/home', { data });
 };
 
