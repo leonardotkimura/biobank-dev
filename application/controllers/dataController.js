b322e48de975d8433003e9e7801c8435fb7021a4
iguau23 <leonardotkimura@gmail.com>
2020-08-06 14:48:45 -0300

    add data index frontend

--- application/controllers/dataController.js
+++ application/controllers/dataController.js
@@ -1,8 +1,24 @@
 const DataContract = require('../contract/dataContract');
+const ControllerUtil = require('./ControllerUtil.js');
 
 exports.index = async function(req, res, next){
+  const data = { 
+    type: ControllerUtil.formatDataType("raw_data"),
+    title: "DNA Sapos da Amazonia", 
+    description: "Esse é um dado muito relevante sobre Sapos da Amazonia",
+    collector: "Indios Tupi Guarani",
+    date: ControllerUtil.formatDate(new Date()),
+    price: "400"
+  }
+
+  const datas = [data,data,data,data,data,data,data,data,data,data,
+                 data,data,data,data,data,data,data,data,data,data,
+                 data,data,data,data,data,data,data,data,data,data,
+                 data,data,data,data,data,data,data,data,data,data,
+                 data,data]
   
-  res.render('data/index', { });
+
+  res.render('data/index', { datas });
 };
 
 
