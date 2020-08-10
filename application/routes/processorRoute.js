0ff77be92dcaec19c7d74325396d7cfffc6c7876
iguau23 <leonardotkimura@gmail.com>
2020-08-10 09:34:50 -0300

    add index layout

---
+++ application/routes/processorRoute.js
@@ -0,0 +1,13 @@
+var router = require('express').Router();
+var ProcessorController = require('../controllers/processorController')
+
+router.get('/', ProcessorController.index);
+router.get('/new', ProcessorController.new);
+router.post('/new', ProcessorController.create);
+router.get('/:processor', ProcessorController.show);
+
+
+
+
+
+module.exports = router;
