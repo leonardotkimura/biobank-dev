49768a780804949604904913d752a5e8300bf298
iguau23 <leonardotkimura@gmail.com>
2020-08-10 11:14:04 -0300

    add new-processor

--- application/controllers/processorController.js
+++ application/controllers/processorController.js
@@ -1,22 +1,11 @@
 const DataContract = require('../contract/dataContract');
 const ControllerUtil = require('./ControllerUtil.js');
 
+
 exports.index = async function(req, res, next){
-  const dataContract = new DataContract();
-  const datas = await dataContract.getAllData();
+  
 
-  const formattedDatas = datas.map(function(data){
-    return {
-      type: ControllerUtil.formatDataType(data.type),
-      title: data.title, 
-      description: data.description,
-      collector: data.collector,
-      created_at: ControllerUtil.formatDate(new Date(data.created_at)),
-      price: data.price
-    }
-  })  
-
-  res.render('processor/index', { datas: formattedDatas });
+  res.render('processor/index', { });
 };
 
 
@@ -26,6 +15,8 @@ exports.new = async function(req, res, next){
 };
 
 exports.create = async function(req, res, next){
+  let processor = getProcessorFromRequest(req);
+  processor.id = ControllerUtil.generateId();
   
   res.render('processor/new', { });
 };
@@ -34,3 +25,10 @@ exports.show = async function(req, res, next){
   
   res.render('processor/show', { });
 };
+
+function getProcessorFromRequest(req){
+  return {
+    name: req.body.name,
+    organization: req.body.organization
+  }
+}
