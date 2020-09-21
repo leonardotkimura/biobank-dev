f6da8442dac0d6c047df1d50a7126af7c3f0478e
iguau23 <leonardotkimura@gmail.com>
2020-09-21 17:59:28 -0300

    add process-request-contract

--- contract/index.js
+++ contract/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const DataContract = require('./lib/data/data-contract');
-const ProcessRequestContract = require('./lib/process-request-contract');
 const ProcessorContract = require('./lib/processor/processor-contract');
 const OperationContract = require('./lib/operation/operation-contract');
+const ProcessRequestContract = require('./lib/process-request/process-request-contract');
 
 module.exports.DataContract = DataContract;
-module.exports.ProcessRequestContract = ProcessRequestContract;
 module.exports.ProcessorContract = ProcessorContract;
 module.exports.OperationContract = OperationContract;
+module.exports.ProcessRequestContract = ProcessRequestContract;
 module.exports.contracts = [ DataContract, ProcessRequestContract, ProcessorContract, OperationContract ];
---
+++ contract/lib/process-request/process-request-contract.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { Contract, Context } = require('fabric-contract-api');
+const ProcessRequest = require('./process-request.js');
+const ProcessRequestList = require('./process-request-list.js');
+
+
+class ProcessRequestContext extends Context {
+    constructor() {
+        super();
+        this.processRequestList = new ProcessRequestList(this);
+    }
+}
+
+class ProcessRequestContract extends Contract {
+    createContext() {
+        return new ProcessRequestContext();
+    }
+
+    async createProcessRequest(ctx, id, processRequestAttributes) {
+        const newProcessRequestAttributes = handleProcessRequestAttributes(id, processRequestAttributes)
+        const processRequest = ProcessRequest.createInstance(newProcessRequestAttributes);
+        await ctx.processRequestList.addProcessRequest(processRequest);
+        return processRequest;
+    }
+
+    async readProcessRequest(ctx, id) {
+        const processRequest = await ctx.processRequestList.getProcessRequest(id);
+        return processRequest;
+    }
+
+    async getAllProcessRequest(ctx) {
+        return await ctx.processRequestList.getAllProcessRequest();
+    }
+}
+
+function handleProcessRequestAttributes(id, processRequestAttributes) {
+    const { raw_data_id, processor_id, processed_data_id, created_at } = JSON.parse(processRequestAttributes);
+    const newOperationAttributes = {
+        id, raw_data_id, processor_id, processed_data_id, created_at
+    }
+    return newOperationAttributes;
+}
+
+module.exports = ProcessRequestContract;
